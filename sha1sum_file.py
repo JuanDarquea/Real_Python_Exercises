8f1347315395d0ef5936a05dcb92719832bf72b8
Juan Darquea <juan.darquea.a@gmail.com>
2025-09-14 01:53:04 -0500

    Practice experience and learning of the usage of sys.argv for file handling and string/int retrieval

---
+++ sha1sum_file.py
@@ -0,0 +1,13 @@
+# sha1sum_file.py
+
+import hashlib
+import sys
+
+def sha1sum(filename: str) -> str:
+    hash = hashlib.sha1()
+    with open(filename, mode="rb") as f:
+        hash.update(f.read())
+    return hash.hexdigest()
+
+for arg in sys.argv[1:]:
+    print(f"{sha1sum(arg)}  {arg}")
