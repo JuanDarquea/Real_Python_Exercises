23353022ba6ff53f4c16e4209ed4364faeaf03b8
Juan Darquea <juan.darquea.a@gmail.com>
2025-09-19 01:47:54 -0500

    The python standard library: argparse and getopt -JD

---
+++ sha1sum_argparse.py
@@ -0,0 +1,50 @@
+# sha1sum_argparse.py
+
+from typing import List
+import hashlib
+import pathlib
+import sys
+import argparse
+
+def process_file(filename: str) -> bytes:
+    return pathlib.Path(filename).read_bytes()
+
+def process_stdin() -> bytes:
+    return bytes("".join(sys.stdin), "utf-8")
+
+def sha1sum(data: bytes) -> str:
+    m = hashlib.sha1()
+    m.update(data)
+    return m.hexdigest()
+
+def output_sha1sum(data: bytes, filename: str = "-") -> None:
+    print(f"{sha1sum(data)}  {filename}")
+
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [OPTION] [FILE]...",
+        description="Print or check SHA1 (160-bit) checksums."
+    )
+    parser.add_argument(
+        "-v", "--version", action="version",
+        version = f"{parser.prog} version 1.0.0"
+    )
+    parser.add_argument('files', nargs='*')
+    return parser
+
+def main(args: List[str]) -> None:
+    parser = init_argparse()
+    args = parser.parse_args()
+    if not args.files:
+        output_sha1sum(process_stdin())
+    for file in args.files:
+        if arg == "-":
+            output_sha1sum(process_stdin(), "-")
+            continue
+        try:
+            output_sha1sum(process_file(file), file)
+        except (FileNotFoundError, IsADirectoryError) as err:
+            print(f"{sys.argv[0]}: {arg}: {err.strerror}", file=sys.stderr)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
